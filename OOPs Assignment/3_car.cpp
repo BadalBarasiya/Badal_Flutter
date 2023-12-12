22eb579e4c8fe15129325a9b3d950ad7c27a0f43
BadalBarasiya <144900492+BadalBarasiya@users.noreply.github.com>
2023-12-12 22:25:47 +0530

    3_car.cpp

--- OOPs Assignment/3_car.cpp
+++ OOPs Assignment/3_car.cpp
@@ -3,40 +3,45 @@ member variables for company, model, and year. Implement member
 functions to get and set these variables.
 */
 
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string>
 using namespace std;
-class Car
-{
-	char company[30];
-	int model;
-	int year;	
-	
-	public :
-void getdata(char c[20],int m, int y){
-	strcpy(company,c);                       // Function to get data
-	model=m;                               
-	year=y;
-}
-void setdata(){
-	cout<<"Company Name : "<<company<<endl;
-	cout<<"Model No : "<<model<<endl;       // Function to set data
-	cout<<"Year : "<<year<<endl;
-}
+
+class Car {
+private:
+    string company;
+    int model;
+    int year;
+
+public:
+    Car(string c, int m, int y) {    // constructor to initialize the value
+	 company=c;
+	  model=m;
+	   year=y; 
+	   }
+
+    void display()  { 
+        cout << "Company Name: " << company << endl;
+        cout << "Model No: " << model << endl;
+        cout << "Year: " << year << endl;
+    }
 };
-int main(){
-	Car obj;              // Create an object of the Car class
-	char c[30];
-	int m, y;
-	cout<<"Company Name : ";
-	cin>>c;
-	cout<<"Model No : ";
-	cin>>m;
-	cout<<"Year : ";
-	cin>>y;
-	
-	obj.getdata(c,m,y);
-	obj.setdata();
-	
-	return 0;
+
+int main() {
+    string c;
+    int m, y;
+
+    cout << "Company Name: ";
+    cin >> c;
+
+    cout << "Model No: ";
+    cin >> m;
+
+    cout << "Year: ";
+    cin >> y;
+
+    Car obj(c, m, y);
+    obj.display();
+
+    return 0;
 }
