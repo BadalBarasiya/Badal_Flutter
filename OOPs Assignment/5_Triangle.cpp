efa50f103e1c6d9a52bb9808dae09abc8d12f5bc
BadalBarasiya <144900492+BadalBarasiya@users.noreply.github.com>
2023-12-12 23:06:42 +0530

    Update 5_Triangle.cpp

--- OOPs Assignment/5_Triangle.cpp
+++ OOPs Assignment/5_Triangle.cpp
@@ -3,42 +3,43 @@ that has private member variables for the lengths of its
 three sides. Implement member functions to determine if 
 the triangle is equilateral, isosceles, or scalene.  */
                                                                            
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Triangle
-{
-	int side1, side2, side3;
-	
-	public : 
-				void getvalue() 
-				{
-				cout<<"\n\n\t Please Enter Three Sides of a Triangle : ";
-				cin>>side1>>side2>>side3;
-				} 
-			
-			void check()
-			{
-				if(side1 == side2 && side2 == side3)     // check the conditions for triangle
-  				{
-  						cout<<"\n\n\t This is an Equilateral Triangle";
-  				}
-  				
-  				else if(side1 == side2 || side2 == side3 || side1 == side3)
-    			{
-  						cout<<"\n\n\t This is an Isosceles Triangle";
-				}
-				
-  				else
-    					cout<<"\n\n\t This is a Scalene Triangle";
-				}
+class Triangle {
+private:
+    double side1, side2,side3;
+
+public:
+    // Constructor to initialize lengths of the sides
+    Triangle(double s1, double s2, double s3) {
+	 side1=s1;
+	  side2=s2;
+	  side3=s3;
+}
+
+    // Function to determine the type of triangle
+    void display() const {
+        if (side1 == side2 && side2 == side3) {
+            cout << "It is an equilateral triangle." << endl;
+        } else if (side1 == side2 || side1 == side3 || side2 == side3) {
+            cout << "It is an isosceles triangle." << endl;
+        } else {
+            cout << "It is a scalene triangle." << endl;
+        }
+    }
 };
 
+int main() {
+    double s1, s2, s3;
 
-main()
-{
-	Triangle T;       // Create an object
-	
-	T.getvalue();
-	T.check();
+    cout << "Enter the lengths of the three sides of the triangle: ";
+    cin >> s1 >> s2 >> s3;
+
+    Triangle T(s1, s2, s3); // Creating a triangle  object
+
+    // Determining and displaying the type of triangle using the triangleType method
+    T.display();
+
+    return 0;
 }
