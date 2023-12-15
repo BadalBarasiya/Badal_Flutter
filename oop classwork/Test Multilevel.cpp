4cfb5597c861e3a7888627e4ec8abb1e0db954bc
BadalBarasiya <144900492+BadalBarasiya@users.noreply.github.com>
2023-12-15 09:16:32 +0530

    Add files via upload

---
+++ oop classwork/Test Multilevel.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class students {
+public:
+    int rollno1, rollno2; // Define roll numbers publicly
+    students() {
+        rollno1 = 32;
+        rollno2 = 35;
+    }
+    void printdata() {
+        cout << "\n\n\t Roll NO1 is " << rollno1;
+        cout << "\n\n\t Roll NO2 is " << rollno2;
+    }
+};
+
+class Test {
+public:
+    int marks1, marks2; // Define test marks publicly
+    Test() {
+        marks1 = 78;
+        marks2 = 65;
+    }
+    void display() {
+        cout << "\n\n\t Marks 1 is " << marks1;
+        cout << "\n\n\t Marks 2 is " << marks2;
+    }
+    int getTotalMarks() {
+        return marks1 + marks2;
+    }
+};
+
+class result : public students, public Test {
+public:
+    result() {} // No need for an explicit constructor here
+    
+    void displayResult() {
+        cout << "\n\n\t Roll NO1 is " << rollno1; // Access roll numbers
+        cout << "\n\n\t Roll NO2 is " << rollno2;
+        
+        cout << "\n\n\t Marks 1 is " << marks1; // Access test marks
+        cout << "\n\n\t Marks 2 is " << marks2;
+        
+        cout << "\n\n\t Total marks is " << getTotalMarks(); // Access total marks
+    }
+};
+
+int main() {
+    result r;
+    r.displayResult(); // Display student info and total marks
+    return 0;
+}
+
